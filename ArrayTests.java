16a8e829666a510e0ac4c976fcf886afd89be09d
David Fontes <dsaldanhadaluzfontes@ucsd.edu>
2024-02-01 13:10:45 -0800

    more
    fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -23,4 +23,19 @@ public class ArrayTests {
     int [] reversedArray = ArrayExamples.reversed(input2);
     assertArrayEquals(new int[]{ 3, 2, 1 }, reversedArray);
   }
+
+  @Test 
+  public void testAverageWithoutLowest(){
+    double[] inputEmpty = {};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(inputEmpty), 0.0001);
+
+    double[] input0 = {1.0,1.0,1.0};
+    assertEquals((1.0), ArrayExamples.averageWithoutLowest(input0), 0.0001);
+    
+    double[] input1 = {1.0,1.0,2.0};
+    assertEquals((1.5), ArrayExamples.averageWithoutLowest(input1), 0.0001);
+
+    double[] input2 = {1.0,2.0,3.0};
+    assertEquals((2.5), ArrayExamples.averageWithoutLowest(input2), 0.0001);
+  }
 }
